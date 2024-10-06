402acdd4e08349225b01a56283be5351e34a1c2c
breno.rodbar <breno.rodbar@gmail.com>
2024-10-06 22:47:52 +0000

    BuildingAdmin

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -117,14 +117,14 @@ const menuItems = [
 
 const Menu = () => {
   return (
-    <div className=''>
+    <div className='mt-4 text-sm'>
       {menuItems.map((i, index)=> (
-        <div className="" key={index}>
-          <span >{i.title}</span>
+        <div className="flex flex-col gap-2" key={index}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
           {i.items.map((item, index)=> (
-            <Link href={item.href} key={index}>
+            <Link href={item.href} key={index} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
             <Image src={item.icon} width={20} height={20} alt=""/>
-            <span>{item.label}</span>
+            <span className="hidden lg:block">{item.label}</span>
             </Link>
           ))}
           </div>
