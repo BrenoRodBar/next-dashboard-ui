31715f03794215212f7c49f3fa15031389e9e3f7
breno.rodbar <breno.rodbar@gmail.com>
2024-10-07 17:19:23 +0000

    Buliding middle chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,20 +1,33 @@
-import UserCard from "@/components/UserCard"
+import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
-    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
       <div className="w-full lg:w-2/3">
         {/* USER CARDS */}
-        <UserCard type="student"/>
-        <UserCard type="teacher"/>
-        <UserCard type="parent"/>
-        <UserCard type="staff"/>
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+
+        {/* MIDDLE CHARTS */}
+
+        <div className=""></div>
+        
+        {/* BOTTOM CHARTS */}
+
+        <div className=""></div>
+        
       </div>
+
       {/* RIGHT */}
+
       <div className="w-full lg:w-1/3"></div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
